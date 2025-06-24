c873bf31116da6c5c3685f6828ac9205c7590001
Kevin95280 <coelhokev@ymail.com>
2025-06-24 16:33:29 +0200

    Constructor setup

---
+++ src/app/models/Recipe.js
@@ -0,0 +1,35 @@
+class Recipe {
+    // Attributs de la classe Recipe
+    #title;
+    #description;
+    #difficulty;
+    #budget;
+    #servings;
+    #preparation_time;
+    #cook_time;
+    #story;
+    #picture;
+    #user_id;
+
+
+    constructor(id, title, description, difficulty, budget, servings, preparation_time, cook_time, story, picture, user_id) {
+        // Initialisation des attributs de la classe Recipe
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.difficulty = difficulty;
+        this.budget = budget;
+        this.servings = servings;
+        this.preparation_time = preparation_time;
+        this.cook_time = cook_time;
+        this.story = story;
+        this.picture = picture;
+        this.user_id = user_id;
+    }
+
+
+
+
+
+
+}
