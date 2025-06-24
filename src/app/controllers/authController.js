941bc0df9f6470b39564e8f3900286ea4ead96f4
Anthony-Hannoteaux <tony.hannoteaux@gmail.com>
2025-06-24 15:47:25 +0200

    Setup feature

---
+++ src/app/controllers/authController.js
@@ -0,0 +1,7 @@
+const authController = {
+    login: (req, res) => {
+        res.render('user-render')
+    }
+}
+
+export default authController;
--- src/app/router.js
+++ src/app/router.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import mainController from './controllers/mainController.js';
+import authController from './controllers/authController.js';
 
 const router = express.Router();
 
 router.get('/', mainController.home)
+router.get('/api/user', authController.login)
 
 export default router;
