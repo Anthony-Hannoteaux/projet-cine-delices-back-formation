866f8ed04bb7742852bff794f674c0106f0822da
Kevin95280 <coelhokev@ymail.com>
2025-06-27 16:00:58 +0200

    fetch API catalogueMovie ok + test

--- src/app/router.js
+++ src/app/router.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import mainController from './controllers/mainController.js';
+import catalogueRoutes from './routes/catalogueRoutes.js';
 
 // Création de notre routeur express
 const router = express.Router();
 
 // Route principale de l'application
 router.get('/', mainController.home)
+
+router.use('/', catalogueRoutes);
 
 export default router;
