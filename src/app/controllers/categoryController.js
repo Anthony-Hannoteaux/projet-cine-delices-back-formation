82bfe0e7e0a6b4d57e8272c05a9e837cc8c6fd4c
youssefyou <youssef.ajdigue@gmail.com>
2025-07-01 16:59:13 +0200

    work in progress, test request

---
+++ src/app/controllers/categoryController.js
@@ -0,0 +1,81 @@
+// import du modèle category
+import Category from "../models/Category.js";
+
+const categoryController = {
+  // utilisation d'une fonction avec une méthode asynchrone pour créer une catégorie
+  createCategory: async (req, res) => {
+    try {
+      // on récupère l'id de notre requête
+      const data = req.body;
+      // on créé une nouvelle instance de Category
+    //   const newCategory = new Category(data.id, data.name);
+      // on utilise la méthode create pour créer une nouvelle catégorie
+    //   const result = await newCategory.create();
+    //   // renvoie une réponse en json
+    //   return res.json(result);
+      // si erreur
+    //   return (data.name, data.id);
+    console.log("name: ", data.name)
+    console.log("name: ", data.id)
+    } catch (error) {
+      console.log("Erreur lors de la création de la catégorie :", error);
+      // alors on renvoie un status 500 avec un message d'erreur
+      return res.status(500).json("Impossible de créer de la catégorie.");
+    }
+  },
+
+  // utilisation d'une fonction avec une méthode asynchrone pour récupérer toutes les catégories
+  getAllCategory: async (req, res) => {
+    try {
+      const data = req.params;
+      const newCategory = new Category(data.id, data.name);
+      // on utilise la méthode findAll pour trouver toutes les catégories
+      const result = await newCategory.findAll();
+      // renvoie une réponse en json
+      return res.json(result);
+      // si erreur
+    } catch (error) {
+      console.log("Erreur lors de la récupération des catégories :", error);
+      // alors on renvoie un status 500 avec un message d'erreur
+      return res.status(500).json("Impossible de récupérer les catégories.");
+    }
+  },
+
+  getCategoryById: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const newCategory = new Category(id);
+      const result = await newCategory.findById();
+      return res.json(result);
+    } catch (error) {
+      console.log("Erreur lors de la récupération de la catégorie :", error);
+      return res.status(500).json("Impossible de récupérer la catégorie.");
+    }
+  },
+
+  updateCategory: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const newCategory = new Category(id);
+      const result = await newCategory.update();
+      return res.json(result);
+    } catch (error) {
+      console.log("Erreur lors de la mise à jour de la catégorie :", error);
+      return res.status(500).json("Impossible de mettre à jour la catégorie.");
+    }
+  },
+
+  deleteCategory: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const newCategory = new Category(id);
+      const result = await newCategory.delete();
+      return res.json(result);
+    } catch (error) {
+      console.log("Erreur lors de la suppression de la catégorie :", error);
+      return res.status(500).json("Impossible de supprimer la catégorie.");
+    }
+  },
+};
+
+export default categoryController;
--- src/app/router.js
+++ src/app/router.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mainController from "./controllers/mainController.js";
 import apiController from "./controllers/apiController.js";
+import categoryController from "./controllers/categoryController.js";
 
 // Création de notre routeur express
 const router = express.Router();
@@ -9,5 +10,13 @@ const router = express.Router();
 router.get("/", mainController.home);
 
 router.get("/catalogue", apiController.catalogue);
+
+// route spécifique aux catégories
+router.post("/api/categories", categoryController.createCategory);
+router.get("/api/categories", categoryController.getAllCategory);
+router.get("/api/categories/:id", categoryController.getCategoryById);
+// patch et non put car on veut modifier une catégorie en particulier
+router.patch("/api/categories/:id", categoryController.updateCategory);
+router.delete("/api/categories/:id", categoryController.deleteCategory);
 
 export default router;
