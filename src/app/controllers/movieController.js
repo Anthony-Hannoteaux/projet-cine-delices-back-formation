a247f3a8fdfe2dba47c8a607ded7f09885f3396d
KimYenNGUYEN <nguyen0kim@hotmail.com>
2025-07-02 11:48:58 +0200

    getAll

--- src/app/controllers/movieController.js
+++ src/app/controllers/movieController.js
@@ -1,6 +1,7 @@
 import Movie from "../models/Movie.js";
 
 const movieController = {
+
     // ajout d'un film
     createMovie: async(req.res) => {
     // bloc try, premier composant de la structure de gestion des erreurs, contient le code à éxécuter tout en surveillant les erreurs potentielles
@@ -9,7 +10,6 @@ const movieController = {
         const {
             TMDB_id, title, overview, poster_path, media_type
         } = req.body;
-
         // création d'une nouvelle instance de Movie avec les données reçues
         const movieToAdd = new Movie(
             TMDB_id, title, overview, poster_path, media_type
@@ -26,6 +26,19 @@ const movieController = {
         res.status(500).json({ error: "Erreur lors de la création du film" });
     }
 },
-}
 
+// lecture de tous les enregistrements de la table "movie"
+getAllMovies: async (req, res) => {
+    try {
+        // appel à la méthode statique findAll() de la classe Movie
+        const allMovies = await Movie.findAll();
+        // renvoi des enregistrements au format JSON, avec un statut 200 pour ok
+        res.status(200).json(allMovies);
+    }
+    catch (error) {
+        res.status(500).json({ error: "Erreur lors de la récupération de tous les films" })
+    }
+},
+} 
+  
 export default movieController;
