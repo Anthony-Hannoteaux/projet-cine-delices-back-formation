4acfba8d4b579dcad35a89cb47fd08f34f1cc5d8
youssefyou <youssef.ajdigue@gmail.com>
2025-07-02 12:55:50 +0200

    add a condition for an existing category to avoid duplication

--- src/app/controllers/categoryController.js
+++ src/app/controllers/categoryController.js
@@ -5,28 +5,32 @@ const categoryController = {
   // utilisation d'une fonction avec une méthode asynchrone pour créer une catégorie
   createCategory: async (req, res) => {
     try {
-    //   console.log("Test de requête POST REQ.BODY :", req.body);
-    //   // on récupère le corps de ma requête grâce à body
-    //   const data = req.body;
-    //   console.log("Test de la requête POST :", data);
-    //   // on créé une nouvelle instance de Category car ce n'est pas une méthode statique
-    //   // appliquable pour la méthode create, update et delete 
-    //   // passe null en argument au lieu de data.id car auto-incrémenté, plus de message d'erreur pour l'id mais le name maintenant
-    //   const newCategory = new Category(data);
-    //   // on utilise la méthode create pour créer une nouvelle catégorie
-    //   const result = await newCategory.create();
-    //   // renvoie une réponse en json
-    //   return res.json(result);
-    //   // si erreur
-    // // console.log("name: ", data.name)
-    // // console.log("name: ", data.id)
-    console.log(req.body);
-    const newCategory = new Category(req.body);
-    console.log(newCategory);
-    newCategory.create();
+      // console.log("Test de requête POST REQ.BODY :", req.body);
+      // on récupère le corps de ma requête grâce à body
+      const data = req.body;
+      // console.log("Test de la requête POST :", data);
+      // on créé une nouvelle instance de Category car ce n'est pas une méthode statique
+      // appliquable pour la méthode create, update et delete 
+      // passe null en argument au lieu de data.id car auto-incrémenté, plus de message d'erreur pour l'id mais le name maintenant
+      
+      // on check si la catégorie est déjà existante dans notre table
+      const existingCategory = await Category.findAll(data.name);
+      // si oui
+      if (existingCategory) {
+        // alors on renvoi un message d'erreur (status 409 : conflit)
+        return res.status(409).json('Cette catégorie existe déjà.');
+      }
+      
+      // sinon on crée une nouvelle catégorie
+      const newCategory = new Category(data);
+      // on utilise la méthode create pour cela
+      const result = await newCategory.create();
+      // renvoie une réponse en json
+      return res.json(result);
+      // si erreur
     } catch (error) {
       console.log("Erreur lors de la création de la catégorie :", error);
-      // alors on renvoie un status 500 avec un message d'erreur
+      // alors on renvoie un status 500 (erreur serveur) avec un message d'erreur
       return res.status(500).json("Impossible de créer de la catégorie.");
     }
   },
