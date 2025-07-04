db1ff64235472ae616afd77918b0fa11efc0a932
Anthony-Hannoteaux <tony.hannoteaux@gmail.com>
2025-07-04 10:45:46 +0200

    bcrypt compare added

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -1,7 +1,25 @@
+import User from "../models/User.js"
+import bcrypt from "bcrypt";
+
 const authController = {
     login: async (req, res) => {
-        console.log(req.body)
-    }
+        try {
+            // Nous commençons par vérifier si l'email est présent dans notre BDD
+            const user = await User.findByEmail(req.body.email)
+            // Si aucun n'utilisateur n'a été créé avec cet email alors
+            if (!user) {
+                // Message générique pour ne pas indiquer plus de précisions sur la donnée incorrecte 
+                return res.status(404).json({ message: "Couple identifiant/mot de passe incorrectes"})
+            }
+            const passwordMatched = await bcrypt.compare(req.body.password, user.password);
+            console.log(passwordMatched)
+            if (!passwordMatched) {
+                return res.status(404).json({ message: "Couple identifiant/mot de passe incorrectes"})
+            }
+        } catch (error) {
+            return res.status(500).json({ message: "Erreur serveur" })
+        }
+    },
 }
 
 export default authController;
