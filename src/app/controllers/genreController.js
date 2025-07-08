613565378c2e8df5a54acc4740f87cc619e01b12
KimYenNGUYEN <nguyen0kim@hotmail.com>
2025-07-08 20:07:17 +0200

    create ok

--- src/app/controllers/genreController.js
+++ src/app/controllers/genreController.js
@@ -6,19 +6,19 @@ const genreController = {
     createGenre: async (req, res) => {
         // bloc try, premier composant de la structure de gestion des erreurs, contient le code à éxécuter tout en surveillant les erreurs potentielles
         try {
-            // récupération des propriétés attendues depuis le corps de la requête qui ne contient que des strings
-            const genreName = req.body.name;
 
-            // création d'une nouvelle instance de Genre avec les données reçues
-            const genreToAdd = new Genre({ genreName })
+            // // création d'une nouvelle instance de Genre avec les données reçues
+            const genreToAdd = new Genre(req.body)
 
             // appel de la méthode d'instance create() pour l'insertion dans la BDD
             const result = await genreToAdd.create();
+            console.log(result)
             // renvoi du résultat au format JSON avec le statut 201 corespondant à "created"
             return res.status(201).json({ message: "Genre créé avec succès", inserted: result });
         }
         // bloc catch, à exécuter en cas d'erreur dans le bloc try
         catch (error) {
+            console.error(error)
             // affichage du message d'erreur avec le statut 500 correspondant à une erreur serveur interne
             return res.status(500).json({ error: "Erreur lors de la création du genre" });
         }
