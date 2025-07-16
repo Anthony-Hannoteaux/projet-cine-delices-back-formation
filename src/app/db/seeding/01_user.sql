e27d2cc52b4716e877fe7e116f8a7d498f02672e
Kevin95280 <coelhokev@ymail.com>
2025-07-16 11:28:53 +0200

    seeding modifications

--- src/app/db/seeding/01_user.sql
+++ src/app/db/seeding/01_user.sql
@@ -2,11 +2,11 @@
 
 BEGIN;
 
-INSERT INTO "user" ("username", "email", "password", "created_at","is_active") VALUES
-    ('chef1', 'chef1@yahoo.fr', 'password1', CURRENT_TIMESTAMP, 'TRUE'),
-    ('chef2', 'chef2@hotmail.fr', 'password2', CURRENT_TIMESTAMP, 'TRUE'),
-    ('chef3', 'chef3@hotmail.fr', 'password3', CURRENT_TIMESTAMP, 'TRUE'),
-    ('chef4', 'chef4@hotmail.fr', 'password4', CURRENT_TIMESTAMP, 'TRUE'),
-    ('chef5', 'chef5@hotmail.fr', 'password5', CURRENT_TIMESTAMP, 'TRUE'),
+INSERT INTO "user" ("username", "email", "password") VALUES
+    ('chef1', 'chef1@yahoo.fr', 'password1'),
+    ('chef2', 'chef2@hotmail.fr', 'password2'),
+    ('chef3', 'chef3@hotmail.fr', 'password3'),
+    ('chef4', 'chef4@hotmail.fr', 'password4'),
+    ('chef5', 'chef5@hotmail.fr', 'password5'),
 
 COMMIT;
